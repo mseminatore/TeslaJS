e8f5a2f880c1c2b8266aadb9c6d01dff97b74173
Mark Seminatore <mseminatore@users.noreply.github.com>
2016-03-15 15:30:00 -0700

    more samples

---
+++ Examples/stop_charge.js
@@ -0,0 +1,60 @@
+//=====================================================================
+// This sample demonstrates the Stop Charging feature using TeslaJS
+//
+// https://github.com/mseminatore/TeslaJS
+//
+// Copyright (c) 2016 Mark Seminatore
+//
+// Refer to included LICENSE file for usage rights and restrictions
+//=====================================================================
+
+var tms = require('../TeslaJS');
+
+//
+//
+//
+function login_cb(result) {
+    if (result.error) {
+        console.error("Login failed!");
+        console.warn(JSON.stringify(result.error));
+        return;
+    }
+
+    var options = { authToken: result.authToken, carIndex: 0 };
+    tms.vehicles(options, function (vehicle) {
+        console.log("Vehicle " + vehicle.vin + " ( '" + vehicle.display_name + "' ) is: " + vehicle.state);
+
+        options.vehicleID = vehicle.id_s;
+        sampleMain(options);
+    });
+}
+
+//
+//
+//
+function sampleMain(options) {
+    tms.stopCharge(options, function (result) {
+        if (result.result)
+            console.log("\nCommand completed successfully!");
+        else
+            console.log(result.reason);
+    });
+}
+
+//
+//
+//
+function usage() {
+    console.log("\nUsage: node <sample_name> <email> <password>\n");
+}
+
+//
+// Sample starts here
+//
+if (process.argv.length < 3) {
+    usage();
+    process.exit(1);
+}
+
+var options = { email: process.argv[2], password: process.argv[3] };
+tms.login(options.email, options.password, login_cb);

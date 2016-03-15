f0ae9b4cface3fd77b56280ba48012980a41a42c
Mark Seminatore <mseminatore@users.noreply.github.com>
2016-03-14 18:09:26 -0700

    updated soc and added odo

--- Examples/odometer.js
+++ Examples/odometer.js
@@ -12,24 +12,23 @@ function login_cb(result) {
     if (result.error)
         console.log(JSON.stringify(result.error));
     else
-        soc(result.authToken);
+        odo(result.authToken);
 }
 
 //
 //
 //
-function soc(token) {
+function odo(token) {
     var options = { authToken: token, carIndex: 0 };
 
     tms.vehicles(options, function (vehicle) {
         console.log("Vehicle " + vehicle.vin + " ( '" + vehicle.display_name + "' ) is: " + vehicle.state);
 
         options.vehicleID = vehicle.id_s;
-        tms.chargeState(options, function (charge_state) {
-            console.log("Current SOC: " + charge_state.battery_level + '%');
-            console.log("Target SOC: " + charge_state.charge_limit_soc + '%');
-            console.log("Rated range: " + Math.round(charge_state.battery_range) + ' miles');
+        tms.vehicleState(options, function (vehicle_state) {
+            console.log("Odometer: " + Math.round(vehicle_state.odometer) + " miles");
         });
+
     });
 }
 

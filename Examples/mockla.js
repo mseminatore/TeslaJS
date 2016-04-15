f776f431888d0f9534fd4491ce57704ae5f463c5
Mark Seminatore <mseminatore@users.noreply.github.com>
2016-04-14 17:10:51 -0700

    added state tracking

--- Examples/mockla.js
+++ Examples/mockla.js
@@ -32,6 +32,8 @@ program
 var vid = 321;
 var username = null;
 var password = null;
+var valetPin = 0;
+var mobileEnabled = true;
 
 var driveState = {
     "shift_state": null,          //
@@ -84,15 +86,15 @@ var vehicleState =
     "pr": false,                  // passenger's side rear door open
     "ft": false,                  // front trunk is open
     "rt": false,                  // rear trunk is open
-    "car_version": "1.19.42",      // car firmware version
+    "car_version": "2.15.16",     // car firmware version
     "locked": true,               // car is locked
-    "sun_roof_installed": false,  // panoramic roof is installed
+    "sun_roof_installed": true,   // panoramic roof is installed
     "sun_roof_state": "unknown",
     "sun_roof_percent_open": 0,   // null if not installed
     "dark_rims": false,           // gray rims installed
     "wheel_type": "Base19",       // wheel type installed
     "has_spoiler": false,         // spoiler is installed
-    "roof_color": "Colored",      // "None" for panoramic roof
+    "roof_color": "None",         // "None" for panoramic roof, "Colored" otherwise
     "perf_config": "Base",
     "vehicle_name": "Kit",        // display name if set
     "valet_mode": false           // true if valet mode is active
@@ -164,7 +166,7 @@ app.get('/api/1/vehicles', function (req, res) {
 // Mock the GET mobile_enabled cmd
 //================================
 app.get('/api/1/vehicles/:vid/mobile_enabled', function (req, res) {
-    res.json({ "response": true });
+    res.json({ "response": mobileEnabled });
 });
 
 //===============================
@@ -223,13 +225,35 @@ app.post('/api/1/vehicles/:vid/wake_up', function (req, res) {
 // Mock the POST set_valet_mode cmd
 //=================================
 app.post('/api/1/vehicles/:vid/command/set_valet_mode', function (req, res) {
-    res.json(resultSuccess);
+    if (req.body.on && valetPin == 0) {
+        vehicleState.valet_mode = true;
+        valetPin = req.body.password;
+        res.json(resultSuccess);
+        return;
+    }
+
+    if (!req.body.on && valetPin == req.body.password) {
+        vehicleState.valet_mode = false;
+        valetPin = 0;
+        res.json(resultSuccess);
+        return;
+    }
+
+    res.json({
+        "response": {
+            "result": false,
+            "reason": "invalid_pin"
+        }
+    });
 });
 
 //==================================
 // Mock the POST reset_valet_pin cmd
 //==================================
 app.post('/api/1/vehicles/:vid/command/reset_valet_pin', function (req, res) {
+    // TODO - does this reset valet mode?
+    vehicleState.valet_mode = false;
+    valetPin = 0;
     res.json(resultSuccess);
 });
 
@@ -237,6 +261,7 @@ app.post('/api/1/vehicles/:vid/command/reset_valet_pin', function (req, res) {
 // Mock the POST charge_port_door_open cmd
 //========================================
 app.post('/api/1/vehicles/:vid/command/charge_port_door_open', function (req, res) {
+    chargeState.charge_port_door_open = true;
     res.json(resultSuccess);
 });
 
@@ -244,18 +269,33 @@ app.post('/api/1/vehicles/:vid/command/charge_port_door_open', function (req, re
 // Mock the POST charge_standard cmd
 //========================================
 app.post('/api/1/vehicles/:vid/command/charge_standard', function (req, res) {
-    res.json({
-        "response": {
-            "result": false,
-            "reason": "already_standard"
-        }
-    });
+    if (chargeState.charge_limit_soc == 90) {
+        res.json({
+            "response": {
+                "result": false,
+                "reason": "already_standard"
+            }
+        });
+    }
+
+    chargeState.charge_limit_soc = 90;
+    res.json(resultSuccess);
 });
 
 //========================================
 // Mock the POST charge_max_range cmd
 //========================================
 app.post('/api/1/vehicles/:vid/command/charge_max_range', function (req, res) {
+    if (chargeState.charge_limit_soc == 100) {
+        res.json({
+            "response": {
+                "result": false,
+                "reason": "already_max_range"
+            }
+        });
+    }
+
+    chargeState.charge_limit_soc = 100;
     res.json(resultSuccess);
 });
 
@@ -264,19 +304,20 @@ app.post('/api/1/vehicles/:vid/command/charge_max_range', function (req, res) {
 //========================================
 app.post('/api/1/vehicles/:vid/command/set_charge_limit', function (req, res) {
     var auth = req.get('Authorization');
-    console.log(auth);
     // TODO - validate the auth token
 
-    var body = req.body;
-    console.log(body.percent);
-
-    // TODO - validate the percent value
+    // ensure valid percent value
+    var percent = req.body.percent;
+    if (percent < 50)
+        percent = 50;
+    if (percent > 100)
+        percent = 100;
 
     // process the state change
-    chargeState.charge_limit_soc = body.percent;
+    chargeState.charge_limit_soc = percent;
 
     // TODO - validate the vehicleID matches
-    console.log(req.params.vid);
+//    console.log(req.params.vid);
 
     res.json(resultSuccess);
 });
@@ -299,6 +340,7 @@ app.post('/api/1/vehicles/:vid/command/charge_stop', function (req, res) {
 // Mock the POST flash_lights cmd
 //========================================
 app.post('/api/1/vehicles/:vid/command/flash_lights', function (req, res) {
+    // no state changes here
     res.json(resultSuccess);
 });
 
@@ -306,6 +348,7 @@ app.post('/api/1/vehicles/:vid/command/flash_lights', function (req, res) {
 // Mock the POST honk_horn cmd
 //========================================
 app.post('/api/1/vehicles/:vid/command/honk_horn', function (req, res) {
+    // no state changes here
     res.json(resultSuccess);
 });
 
@@ -313,6 +356,7 @@ app.post('/api/1/vehicles/:vid/command/honk_horn', function (req, res) {
 // Mock the POST door_unlock cmd
 //========================================
 app.post('/api/1/vehicles/:vid/command/door_unlock', function (req, res) {
+    vehicleState.locked = false;
     res.json(resultSuccess);
 });
 
@@ -320,6 +364,7 @@ app.post('/api/1/vehicles/:vid/command/door_unlock', function (req, res) {
 // Mock the POST door_lock cmd
 //========================================
 app.post('/api/1/vehicles/:vid/command/door_lock', function (req, res) {
+    vehicleState.locked = true;
     res.json(resultSuccess);
 });
 
@@ -327,6 +372,14 @@ app.post('/api/1/vehicles/:vid/command/door_lock', function (req, res) {
 // Mock the POST set_temps cmd
 //========================================
 app.post('/api/1/vehicles/:vid/command/set_temps', function (req, res) {
+    var driverTemp = req.body.driver_temp;
+    var passTemp = req.body.passenger_temp;
+
+    // TODO - clamp temp values to appropriate ranges
+
+    climateState.driver_temp_setting = driverTemp;
+    climateState.passenger_temp_setting = passTemp;
+
     res.json(resultSuccess);
 });
 
@@ -334,6 +387,8 @@ app.post('/api/1/vehicles/:vid/command/set_temps', function (req, res) {
 // Mock the POST auto_conditioning_start cmd
 //========================================
 app.post('/api/1/vehicles/:vid/command/auto_conditioning_start', function (req, res) {
+    climateState.is_auto_conditioning_on = true;
+    climateState.fan_status = 6;
     res.json(resultSuccess);
 });
 
@@ -341,6 +396,8 @@ app.post('/api/1/vehicles/:vid/command/auto_conditioning_start', function (req, 
 // Mock the POST auto_conditioning_stop cmd
 //========================================
 app.post('/api/1/vehicles/:vid/command/auto_conditioning_stop', function (req, res) {
+    climateState.is_auto_conditioning_on = false;
+    climateState.fan_status = 0;
     res.json(resultSuccess);
 });
 
@@ -348,6 +405,10 @@ app.post('/api/1/vehicles/:vid/command/auto_conditioning_stop', function (req, r
 // Mock the POST sun_roof_control cmd
 //========================================
 app.post('/api/1/vehicles/:vid/command/sun_roof_control', function (req, res) {
+    vehicleState.sun_roof_percent_open = req.body.percent;
+
+    // TODO - clamp percent values
+
     res.json(resultSuccess);
 });
 

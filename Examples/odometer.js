22a732de096e3e8616ef54ec11d9e3eebf2fa608
unknown <mseminatore@users.noreply.github.com>
2016-08-12 21:06:19 -0700

    improved output formatting

--- Examples/odometer.js
+++ Examples/odometer.js
@@ -44,9 +44,32 @@ function login_cb(result) {
 //
 //
 //
+function addCommas(str)
+{
+	str += '';
+	x = str.split('.');
+	x1 = x[0];
+	x2 = x.length > 1 ? '.' + x[1] : '';
+	var rgx = /(\d+)(\d{3})/;
+	while (rgx.test(x1)) {
+		x1 = x1.replace(rgx, '$1' + ',' + '$2');
+	}
+	return x1 + x2;
+}
+
+//
+//
+//
 function sampleMain(options) {
     tjs.vehicleState(options, function (vehicle_state) {
-        console.log("\nOdometer: " + Math.round(vehicle_state.odometer).toString().green + " miles");
+        console.log("\nOdometer");
+		console.log("--------\n");
+		
+		var miles = addCommas(Math.round(vehicle_state.odometer).toString());
+		console.log(miles.green + " miles");
+		
+		var km = addCommas(Math.round(vehicle_state.odometer * 1.61).toString());
+		console.log(km.green + " KM");
     });
 }
 

7155f57352cb68d0c9ce26a8681f3810c22a7703
unknown <mseminatore@users.noreply.github.com>
2016-11-20 22:46:22 -0800

    started adding mocha test support

---
+++ test/test.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+var tjs = require('../teslajs');
+
+describe('TeslaJS', function() {
+	describe('#login()', function() {
+		it('should succeed with valid pwd', function() {
+			assert(true);
+		});
+		it('should fail with invalid pwd', function() {
+			assert(true);
+		});
+	});
+});

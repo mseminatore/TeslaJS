e5fa2760ed1e13db5f046177a3f643b4c7078ba3
unknown <mseminatore@users.noreply.github.com>
2017-01-05 20:28:59 -0800

    fixed jshint errors

--- Examples/vehicle.js
+++ Examples/vehicle.js
@@ -37,12 +37,24 @@ function sampleMain(tjs, options) {
         console.log("\nVehicle name: " + vehicle_state.vehicle_name.green);
 
         console.log("\nDoors: " + str);
-        vehicle_state.df ? console.log("Driver door: " + "OPEN".red) : "";
-        vehicle_state.pf ? console.log("Passenger door: " + "OPEN".red) : "";
-        vehicle_state.dr ? console.log("Driver side rear door: " + "OPEN".red) : "";
-        vehicle_state.prf ? console.log("Passenger side rear door: " + "OPEN".red) : "";
-        vehicle_state.ft ? console.log("Front trunk: " + "OPEN".red) : "";
-        vehicle_state.rt ? console.log("Rear trunk: " + "OPEN".red) : "";
+        if (vehicle_state.df) {
+            console.log("Driver door: " + "OPEN".red);
+        } 
+        if (vehicle_state.pf) {
+            console.log("Passenger door: " + "OPEN".red);
+        }
+        if (vehicle_state.dr) {
+            console.log("Driver side rear door: " + "OPEN".red);
+        }
+        if (vehicle_state.pr) {
+            console.log("Passenger side rear door: " + "OPEN".red);
+        }
+        if (vehicle_state.ft) {
+            console.log("Front trunk: " + "OPEN".red);
+        }
+        if (vehicle_state.rt) {
+            console.log("Rear trunk: " + "OPEN".red);
+        }
 
         if (vehicle_state.sun_roof_installed) {
             var state = "CLOSED";

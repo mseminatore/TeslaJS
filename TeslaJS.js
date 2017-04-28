7f2ae71a6dfe023d72b4396fe672f57febe3db05
Thomas Bensmann <thomas@bensmann.no>
2017-04-28 11:51:18 +0200

    Removed typo

--- TeslaJS.js
+++ TeslaJS.js
@@ -686,7 +686,7 @@ exports.post_commandAsync = Promise.denodeify(exports.post_command);
  * @returns {object} vehicle_data object
  */
 exports.vehicleData = function vehicleData(options, callback){
-    tesla.get_command(options, "data", callback);
+    get_command(options, "data", callback);
 }
 
 /**
